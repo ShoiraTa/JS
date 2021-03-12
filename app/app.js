50e912e2043f4766fe65fc2d29e13cd84d36c43d
shoirata <shoira.shakirovna@gmail.com>
2021-03-12 17:17:20 +0500

    looping arr

--- app/app.js
+++ app/app.js
@@ -1,26 +1,84 @@
+//DO WHILE///////////////////////////
 // var myArray = [];
 // var i = 10;
-
-// // Only change code below this line
 // do{
 // myArray.push(i);
 // i++;
 // }
-// while (i < 5) 
+// while (i < 5)
 // console.log(myArray);
 
-// Recursion
+// RECURSION///////////////////////////
 
-function recursion (val) {
-  var result = val;
-  if (val == 0 && val == 1) {
-    return 1;
-  } while (val > 1) {
-    result = result * (val - 1);
-    val--;
+// function rec(val){
+//      result = val
+//      if(val ==0 || val ==1){
+//           return 1
+//      }
+//      while (val>1){
+//      result = result * (val - 1);
+//      val --
+//      }
+//      return result
+// }
+
+// console.log (rec(5))               
+
+// Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+// function sum(arr, n) {
+//   if(n <=0){
+//        return 0
+//   }
+//   else {
+//        return sum(arr, n - 1) + arr[n - 1];
+//   }
+
+// }
+
+// console.log(sum([2, 3, 4, 5], 3));
+
+var contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+     for(var i =0; i<contacts.length; i++){
+       
+     if (contacts[i].firstName === name) {
+      
+       if (contacts[i].hasOwnProperty(prop)) {
+         return contacts[i][prop];
+       } else {
+         return "No such property";
+       }
+     }
   }
-  return result;
-  
+  return "No such contact";   
+
 }
 
-console.log(recursion(5));
+console.log(lookUpProfile("Akira", "likes"));
