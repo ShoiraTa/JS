a35e59893c99d0ff37e1224da06da39e193e51dc
shoirata <shoira.shakirovna@gmail.com>
2021-03-12 15:48:29 +0500

    recursion

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
-// Setup
-var myArray = [];
-var i = 10;
+// var myArray = [];
+// var i = 10;
 
-// Only change code below this line
-do{
-myArray.push(i);
-i++;
+// // Only change code below this line
+// do{
+// myArray.push(i);
+// i++;
+// }
+// while (i < 5) 
+// console.log(myArray);
+
+// Recursion
+
+function recursion (val) {
+  var result = val;
+  if (val == 0 && val == 1) {
+    return 1;
+  } while (val > 1) {
+    result = result * (val - 1);
+    val--;
+  }
+  return result;
+  
 }
-while (i < 5) 
-console.log(myArray);
+
+console.log(recursion(5));
