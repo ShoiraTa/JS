30daa65a88a632bb92bf94e91d2885019dbee67a
shoirata <shoira.shakirovna@gmail.com>
2021-03-12 17:21:29 +0500

    math

--- app/app.js
+++ app/app.js
@@ -38,47 +38,52 @@
 
 // console.log(sum([2, 3, 4, 5], 3));
 
-var contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+// var contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
 
-function lookUpProfile(name, prop) {
-     for(var i =0; i<contacts.length; i++){
+// function lookUpProfile(name, prop) {
+//      for(var i =0; i<contacts.length; i++){
        
-     if (contacts[i].firstName === name) {
+//      if (contacts[i].firstName === name) {
       
-       if (contacts[i].hasOwnProperty(prop)) {
-         return contacts[i][prop];
-       } else {
-         return "No such property";
-       }
-     }
-  }
-  return "No such contact";   
+//        if (contacts[i].hasOwnProperty(prop)) {
+//          return contacts[i][prop];
+//        } else {
+//          return "No such property";
+//        }
+//      }
+//   }
+//   return "No such contact";   
 
+// }
+
+// console.log(lookUpProfile("Akira", "likes"));
+function randomWholeNum() {
+  Math.floor(Math.random()*9)
+
+  return Math.random();
 }
-
-console.log(lookUpProfile("Akira", "likes"));
